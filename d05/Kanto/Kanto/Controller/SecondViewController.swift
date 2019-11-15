16018e2f1931442fe8bd26f611a7cef4d16ad08f
stevevovchyna <steve.vovchyna@gmail.com>
2019-11-15 17:41:02 +0200

    d05 ex04 complete

--- d05/Kanto/Kanto/Controller/SecondViewController.swift
+++ d05/Kanto/Kanto/Controller/SecondViewController.swift
@@ -8,19 +8,35 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var currentLatitude : Double?
+    var currentLongitude : Double?
   
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var selector: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        map.showsUserLocation = false
+
         // Do any additional setup after loading the view.
         addPin(latitude: 48.896607, longitude: 2.318501, title: "Ecole 42", subtitle: "There's no place like home")
         focusMapView(lat: 48.896607, lon: 2.318501)
     }
 
+    @objc func stopLocating() {
+        map.showsUserLocation = false
+    }
+    
     @IBAction func changeMapView(_ sender: UISegmentedControl) {
         switch selector.selectedSegmentIndex {
         case 0:
@@ -31,6 +47,22 @@ class SecondViewController: UIViewController {
             map.mapType = .hybrid
         default:
             return
+        }
+    }
+    @IBAction func getUserLocation(_ sender: UIButton) {
+        
+        map.showsUserLocation = true
+        focusMapView(lat: currentLatitude ?? 30.0, lon: currentLongitude ?? 40.0)
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            
+            currentLatitude = Double(location.coordinate.latitude)
+            currentLongitude = Double(location.coordinate.longitude)
+            
         }
     }
     
@@ -44,7 +76,7 @@ class SecondViewController: UIViewController {
     
     func focusMapView(lat : Double, lon : Double) {
         let mapCenter = CLLocationCoordinate2DMake(lat, lon)
-        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: mapCenter, span: span)
         map.region = region
     }
