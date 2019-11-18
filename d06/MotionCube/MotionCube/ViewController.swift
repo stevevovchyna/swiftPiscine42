2c13a80d5ea8e721f07edcb912c56eedda27b4fd
stevevovchyna <steve.vovchyna@gmail.com>
2019-11-18 13:50:03 +0200

    d05 - constraints fixed, d06 - initializwd

---
+++ d06/MotionCube/MotionCube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionCube
+//
+//  Created by Steve Vovchyna on 18.11.2019.
+//  Copyright © 2019 Steve Vovchyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
