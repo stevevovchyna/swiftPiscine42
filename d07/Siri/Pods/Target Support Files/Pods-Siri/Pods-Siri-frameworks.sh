1629ef3d57f9328b5ed89f4fe6cbc547af98c8af
stevevovchyna <steve.vovchyna@gmail.com>
2019-11-21 23:46:10 +0200

    d07ex03 complete

--- d07/Siri/Pods/Target Support Files/Pods-Siri/Pods-Siri-frameworks.sh
+++ d07/Siri/Pods/Target Support Files/Pods-Siri/Pods-Siri-frameworks.sh
@@ -162,11 +162,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DarkSkyKit/DarkSkyKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/sapcai/sapcai.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DarkSkyKit/DarkSkyKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/sapcai/sapcai.framework"
 fi
--- d07/Siri/Siri/FirstViewController.swift
+++ d07/Siri/Siri/FirstViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import sapcai
+import DarkSkyKit
 
 class FirstViewController: UIViewController {
 
     var bot = SapcaiClient(token : "0dedc07f4cf04e658b15ad041b2a5feb", language: "en")
+    let forecastClient = DarkSkyKit(apiToken: "2b485393cb19279ada9959ed78f14c7a")
     
     @IBOutlet weak var responseLabel: UILabel!
     @IBOutlet weak var textInput: UITextField!
@@ -23,13 +25,30 @@ class FirstViewController: UIViewController {
     }
     
     @IBAction func sendButtonPressed(_ sender: Any) {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.insertSubview(blurEffectView, at: 0)
         if let request = textInput.text {
             self.bot.analyseText(request, successHandler: { (response) in
-                print(response.toJSONString()! as Any)
-                if response.entities?.toJSON().count == 0 {
-                    self.responseLabel.text = "Error getting data with your request"
+//                print(response.toJSONString()! as Any)
+                if response.entities?.locations == nil {
+                    self.responseLabel.text = "Error getting weather data with your request"
                 } else if let jsonResponse = response.entities?.locations?[0].toJSON() {
-                    self.responseLabel.text = ("The location is \(String(describing: jsonResponse["lat"]!)) and \(String(describing: jsonResponse["lng"]!))")
+                    self.forecastClient.current(latitude: Double(jsonResponse["lat"] as! Float), longitude: Double(jsonResponse["lng"] as! Float)) { result in
+                      switch result {
+                        case .success(let forecast):
+                            if let current = forecast.currently {
+//                            print(forecast.currently as Any)
+                                blurEffectView.removeFromSuperview()
+                            self.responseLabel.text = ("It is \(String(describing: ((current.temperature! - 32) * (5 / 9)).rounded())) ℃ and \(current.icon!) in \(String(describing: jsonResponse["raw"]!))")
+                                
+                        }
+                        case .failure(let error):
+                            print(error)
+                      }
+                    }
                 }
                 
             }) { (error) in
