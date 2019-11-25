f066778121bc474e27e218f25dea74285b435c3c
stevevovchyna <steve.vovchyna@gmail.com>
2019-11-25 13:34:55 +0200

    d09init, ex00 complete - pod installed

---
+++ d09/Personal Diary/Personal Diary/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Personal Diary
+//
+//  Created by Steve Vovchyna on 25.11.2019.
+//  Copyright © 2019 Steve Vovchyna. All rights reserved.
+//
+
+import UIKit
+import svovchyn2019
+
+class ViewController: UIViewController {
+    
+    var articleManager = ArticleManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ d09/Personal Diary/Pods/Target Support Files/svovchyn2019/svovchyn2019-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_svovchyn2019 : NSObject
+@end
+@implementation PodsDummy_svovchyn2019
+@end
